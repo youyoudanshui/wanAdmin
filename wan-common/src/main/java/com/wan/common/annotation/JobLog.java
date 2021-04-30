12a9e85d7ce1d0ec1f5e10fe1c03bab0510584e2
youyoudanshui <591483809@qq.com>
2021-05-01 00:34:37 +0800

    v1.0.0

--- wan-common/src/main/java/com/wan/common/annotation/JobLog.java
+++ wan-common/src/main/java/com/wan/common/annotation/JobLog.java
@@ -6,8 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.wan.common.enumerate.OperationType;
-
 /**
  * 自定义操作定时任务日志注解
  * @author wmj
